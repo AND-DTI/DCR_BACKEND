15c2778c1676afeb1bec1a558200a78a90126f6a
Guilherme Alves Rodrigues Inacio <SBP17096@sa.mds.honda.com>
2024-02-05 08:43:55 -0400

    Criação do controller de matriz de documento

---
+++ src/main/java/com/dcr/api/model/as400/Matridoc.java
@@ -0,0 +1,201 @@
+package com.dcr.api.model.as400;
+
+import com.dcr.api.model.keys.MatridocKey;
+import com.dcr.api.validator.TamanhoMaximo;
+import com.dcr.api.validator.TamanhoMinimo;
+
+import io.swagger.annotations.ApiModel;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "MATRIDOC", schema = "HD4DCDHH")
+@ApiModel
+public class Matridoc {
+
+	@EmbeddedId
+	private MatridocKey key;
+	
+	@TamanhoMaximo(20)
+	@TamanhoMinimo(1)
+	@Column(columnDefinition = "char(20)")
+	private String numdoc;
+	
+	@TamanhoMaximo(10)
+	@TamanhoMinimo(1)
+	@Column(columnDefinition = "char(10)")
+	private String serdoc;
+	
+	@TamanhoMaximo(8)
+	@TamanhoMinimo(1)
+	@Column(columnDefinition = "char(8)")
+	private String emidoc;
+	
+	@TamanhoMaximo(20)
+	@TamanhoMinimo(1)
+	@Column(columnDefinition = "char(20)")
+	private String numdoc2;
+
+	@TamanhoMaximo(10)
+	@TamanhoMinimo(1)
+	@Column(columnDefinition = "char(10)")
+	private String serdoc2;
+	
+	@TamanhoMaximo(8)
+	@TamanhoMinimo(1)
+	@Column(columnDefinition = "char(8)")
+	private String emidocnew;
+	
+	@TamanhoMaximo(20)
+	@TamanhoMinimo(1)
+	@Column(columnDefinition = "char(20)")
+	private String numdocnew;
+	
+	@TamanhoMaximo(10)
+	@TamanhoMinimo(1)
+	@Column(columnDefinition = "char(10)")
+	private String serdocnew;
+	
+	@TamanhoMaximo(8)
+	@TamanhoMinimo(1)
+	@Column(columnDefinition = "char(8)")
+	private String emidoc2;
+	
+	
+	@Column(columnDefinition = "char(40)")
+    private String itaudsys;	
+	
+	@Column(columnDefinition = "char(10)")
+    private String itaudusr;
+	
+	@Column(columnDefinition = "char(20)")
+    private String itaudhst;
+	
+	@Column(columnDefinition = "char(8)")
+    private String itauddt;
+	
+	@Column(columnDefinition = "char(8)")
+    private String itaudhr;
+
+	public MatridocKey getKey() {
+		return key;
+	}
+
+	public void setKey(MatridocKey key) {
+		this.key = key;
+	}
+
+	public String getNumdoc() {
+		return numdoc;
+	}
+
+	public void setNumdoc(String numdoc) {
+		this.numdoc = numdoc;
+	}
+
+	public String getSerdoc() {
+		return serdoc;
+	}
+
+	public void setSerdoc(String serdoc) {
+		this.serdoc = serdoc;
+	}
+
+	public String getEmidoc() {
+		return emidoc;
+	}
+
+	public void setEmidoc(String emidoc) {
+		this.emidoc = emidoc;
+	}
+
+	public String getNumdoc2() {
+		return numdoc2;
+	}
+
+	public void setNumdoc2(String numdoc2) {
+		this.numdoc2 = numdoc2;
+	}
+
+	public String getSerdoc2() {
+		return serdoc2;
+	}
+
+	public void setSerdoc2(String serdoc2) {
+		this.serdoc2 = serdoc2;
+	}
+
+	public String getEmidocnew() {
+		return emidocnew;
+	}
+
+	public void setEmidocnew(String emidocnew) {
+		this.emidocnew = emidocnew;
+	}
+
+	public String getNumdocnew() {
+		return numdocnew;
+	}
+
+	public void setNumdocnew(String numdocnew) {
+		this.numdocnew = numdocnew;
+	}
+
+	public String getSerdocnew() {
+		return serdocnew;
+	}
+
+	public void setSerdocnew(String serdocnew) {
+		this.serdocnew = serdocnew;
+	}
+
+	public String getEmidoc2() {
+		return emidoc2;
+	}
+
+	public void setEmidoc2(String emidoc2) {
+		this.emidoc2 = emidoc2;
+	}
+
+	public String getItaudsys() {
+		return itaudsys;
+	}
+
+	public void setItaudsys(String itaudsys) {
+		this.itaudsys = itaudsys;
+	}
+
+	public String getItaudusr() {
+		return itaudusr;
+	}
+
+	public void setItaudusr(String itaudusr) {
+		this.itaudusr = itaudusr;
+	}
+
+	public String getItaudhst() {
+		return itaudhst;
+	}
+
+	public void setItaudhst(String itaudhst) {
+		this.itaudhst = itaudhst;
+	}
+
+	public String getItauddt() {
+		return itauddt;
+	}
+
+	public void setItauddt(String itauddt) {
+		this.itauddt = itauddt;
+	}
+
+	public String getItaudhr() {
+		return itaudhr;
+	}
+
+	public void setItaudhr(String itaudhr) {
+		this.itaudhr = itaudhr;
+	}
+}
---
+++ src/main/java/com/dcr/api/model/dto/MatridocDTO.java
@@ -0,0 +1,6 @@
+package com.dcr.api.model.dto;
+
+public record MatridocDTO(Integer idmatriz,String partnum,String tpdoc,String numdoc,String serdoc,String emidoc,String numdoc2,String serdoc2,String emidocnew,String numdocnew,String serdocnew,String emidoc2
+		) {
+
+}
